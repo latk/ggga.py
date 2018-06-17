604ac16b242e1bf64412c917c5a46f12f8859d82
Lukas Atkinson <fra-uas@LukasAtkinson.de>
2018-06-17 23:16:48 +0200

    EI-guided chains

--- ggga.py
+++ ggga.py
@@ -7,6 +7,7 @@ import abc
 from skopt.learning import GaussianProcessRegressor  # type: ignore
 from skopt.learning.gaussian_process.kernels import (  # type: ignore
         ConstantKernel, Matern, WhiteKernel)
+import scipy.stats  # type: ignore
 import typing as t
 
 T = t.TypeVar('T')
@@ -56,12 +57,13 @@ class Integer(Param[int]):
         return rng.randint(self.lo, self.hi + 1)
 
     def mutate(self, value, *, rng: RandomState, relscale: float) -> int:
-        retries = 10
+        retries = 20
         x = self.into_transformed(value)
         for _ in range(retries):
             mutx = x + rng.standard_normal() * relscale
             if self.is_valid_transformed(mutx):
                 return self.from_transformed(mutx)
+            relscale *= 0.8
         raise RuntimeError("mutation failed to produce values within bounds")
 
     @property
@@ -91,12 +93,13 @@ class Real(Param[float]):
         return rng.random_sample() * self.size + self.lo
 
     def mutate(self, value, *, rng: RandomState, relscale: float) -> float:
-        retries = 10
+        retries = 20
         x = self.into_transformed(value)
         for _ in range(retries):
             mutx = x + rng.standard_normal() * relscale
             if self.is_valid_transformed(mutx):
                 return self.from_transformed(mutx)
+            relscale *= 0.8
         raise RuntimeError("mutation failed to produce values within bounds")
 
     @property
@@ -219,8 +222,16 @@ class SurrogateModel(object):
 
 
 class Logger(object):
-    def log_evaluation(self, x, y) -> None:
-        print("[INFO] evaluated {} to {}".format(x, y))
+    def record_evaluations(self, individuals, *, space):
+        print("[INFO] evaluations:")
+        param_names = [p.name for p in space.params]
+        param_formats = \
+            ['{:.5f}' if isinstance(p, Real) else '{}' for p in space.params]
+        print(tabularize(
+            header=['utility', *param_names],
+            formats=['{:.2f}', *param_formats],
+            data=[[ind.fitness, *ind.sample] for ind in individuals],
+        ))
 
     def announce_new_generation(
         self, gen: int, *,
@@ -238,14 +249,24 @@ class Individual(object):
         self.fitness = fitness
 
 
+def expected_improvement(mean, std, fmin):
+    norm = scipy.stats.norm
+    z = -(mean - fmin) / std
+    ei = -(mean - fmin) * norm.cdf(z) + std * norm.pdf(z)
+    return -ei
+
+
 def minimize(
-        objective: t.Callable[[list, RandomState], float],
-        *,
-        space: Space,
-        popsize: int=10,
-        max_nevals: int=100,
-        logger: Logger=None,
-        rng: RandomState):
+    objective: t.Callable[[list, RandomState], float],
+    *,
+    space: Space,
+    popsize: int=10,
+    max_nevals: int=100,
+    logger: Logger=None,
+    rng: RandomState,
+    relscale_initial=0.3,
+    relscale_attenuation=0.9,
+):
 
     if logger is None:
         logger = Logger()
@@ -262,52 +283,100 @@ def minimize(
     for ind in population:
         ind.fitness = objective(ind.sample, rng)
         all_evaluations.append(ind)
-        logger.log_evaluation(ind.sample, ind.fitness)
+    logger.record_evaluations(population, space=space)
 
     model = SurrogateModel.estimate(
         [ind.sample for ind in all_evaluations],
         [ind.fitness for ind in all_evaluations],
         space=space, rng=rng)
 
+    def suggest_next_candidate(
+        parent_sample, *, relscale, fmin, rng, breadth, model,
+    ):
+        candidate_samples = [
+            space.mutate(parent_sample, relscale=relscale, rng=rng)
+            for _ in range(breadth)]
+        candidate_mean, candidate_std = model.predict_a(candidate_samples)
+        candidate_ei = expected_improvement(
+            candidate_mean, candidate_std, fmin)
+        return candidate_samples[np.argmax(candidate_ei)]
+
     generation = 0
     while len(all_evaluations) < max_nevals:
         generation += 1
-        relscale = 0.1 * (0.9 ** (generation - 1))
+        relscale = relscale_initial * (relscale_attenuation**(generation - 1))
 
         logger.announce_new_generation(
             generation, model=model, relscale=relscale)
 
         # generate new individuals
-        new: t.List[Individual] = []
-        retries = 5
+        offspring: t.List[Individual] = []
         for parent in population:
-            for _ in range(retries):
-                child = Individual(
-                    space.mutate(parent.sample, relscale=relscale, rng=rng),
-                    None,
+            candidate_sample = parent.sample
+            candidate_chain_length = 5
+            for i in range(candidate_chain_length):
+                candidate_sample = suggest_next_candidate(
+                    candidate_sample,
+                    relscale=relscale * (relscale_attenuation ** i),
+                    fmin=parent.fitness,
+                    rng=rng,
+                    breadth=20,
+                    model=model,
                 )
-                # TODO check for EI instead of blind minimization
-                child_prediction, parent_prediction = \
-                    model.predict_a([child.sample, parent.sample])[0]
-                if child_prediction < parent_prediction:
-                    new.append(child)
-                    break
+            offspring.append(Individual(candidate_sample, None))
 
         # evaluate new individuals
-        for ind in new:
+        for ind in offspring:
             ind.fitness = objective(ind.sample, rng)
             all_evaluations.append(ind)
-            logger.log_evaluation(ind.sample, ind.fitness)
-        population.extend(new)
+        logger.record_evaluations(offspring, space=space)
 
-        # select new population
+        # fit next model
         model = SurrogateModel.estimate(
             [ind.sample for ind in all_evaluations],
             [ind.fitness for ind in all_evaluations],
             space=space, rng=rng)
-        pop_predictions, pop_predictions_std = \
-            model.predict_a([ind.sample for ind in population])
-        selection_index = np.argsort(pop_predictions)[:popsize]
-        population = [population[i] for i in selection_index]
 
-    return population[0].sample, population[0].fitness
+        # select new population
+        selected = []
+        rejected = []
+        for i in range(popsize):
+            a, b = offspring[i], population[i]
+            if b.fitness < a.fitness:
+                a, b = b, a
+            selected.append(a)
+            rejected.append(b)
+        selected = [
+            selected[i] for i in np.argsort([ind.fitness for ind in selected])
+        ]
+
+        # replace worst selected elements
+        replace_worst_n = popsize - 3
+        replacement_locations = \
+            np.argsort([ind.fitness for ind in rejected])[:replace_worst_n]
+        for i, j in zip(worst_locations, replacement_locations):
+            selected[i] = rejected[j]
+
+        population = selected
+
+    best_i = np.argmin([ind.fitness for ind in population])
+    return population[best_i].sample, population[best_i].fitness
+
+
+def tabularize(
+        header: t.List[str],
+        formats: t.List[str],
+        data: t.Iterable[list],
+) -> str:
+    columns = [[str(h)] for h in header]
+    for row in data:
+        for col, f, d in zip(columns, formats, row):
+            col.append(f.format(d))
+    col_size = [max(len(d) for d in col) for col in columns]
+    out = []
+    out.append(' '.join('-' * size for size in col_size))
+    for i in range(len(columns[0])):
+        out.append(' '.join(
+            col[i].rjust(size) for col, size in zip(columns, col_size)))
+    out[0], out[1] = out[1], out[0]
+    return '\n'.join(out)
